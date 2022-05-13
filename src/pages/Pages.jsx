1335f34c3bf2de2dd05db36bf862d43f66785e9f
IHECHI <nnatahi57@gmail.com>
2022-05-13 02:10:18 +0100

    update

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Home from './Home';
+import {Route, Routes, useLocation} from 'react-router-dom';
+import Cuisine from './Cuisine'
+import Searched from './Searched';
+import Recipe from './Recipe';
+import {AnimatePresence} from 'framer-motion';
+
+
+function Pages() {
+  const location = useLocation();
+  return (
+   <AnimatePresence exitBeforeEnter>
+    <Routes Location={location} key={location.pathname}>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Cuisine/:type" element={<Cuisine/>}/>
+        <Route path="/searched/:search" element={<Searched/>}/>
+        <Route path="/Recipe/:name" element={<Recipe/>}/>
+    </Routes>
+    // </AnimatePresence>
+  );
+}
+
+export default Pages
+
